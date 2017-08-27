3711ce7580597094914eb35a103ef045d9e88271
Jelgar <jonathanelgardiner@gmail.com>
2017-08-28 00:52:00 +0100

    WIP w API service

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { NameListService } from '../shared/name-list/name-list.service';
+import { HttpModule }    from '@angular/http';
+import { ApiService} from '../services/api.service';
+
 
 @NgModule({
   imports: [HomeRoutingModule, SharedModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
-  providers: [NameListService]
+  providers: [HttpModule, ApiService]
 })
 export class HomeModule { }
