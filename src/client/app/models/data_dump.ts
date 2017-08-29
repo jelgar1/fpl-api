ed6ff6854cd382a140b859cb41fcd5b72c554cee
Jelgar <jonathanelgardiner@gmail.com>
2017-08-30 00:47:10 +0100

    added basic player model

--- src/client/app/models/data_dump.ts
+++ src/client/app/models/data_dump.ts
@@ -1,7 +1,11 @@
+import { Player } from './player';
+
 export class DataDump {
   players: object[];
 
   static fromJSON(obj: any): DataDump {
-    return Object.assign(Object.create(DataDump.prototype), obj, {});
+    let newObject = Object.assign(Object.create(DataDump.prototype), obj, {});
+    if (newObject['elements']) newObject['elements'] = (newObject['elements'] || []).map((raw: any) => Player.fromJSON(raw));
+    return newObject;
   }
 }
